e0845d873700d78ed610eba57e43f58d04db948f
Steve Bertrand <steveb@cpan.org>
2018-03-13 17:41:15 +0000

    added examples/module_test.pl

---
+++ examples/module_test.pl
@@ -0,0 +1,22 @@
+use warnings;
+use strict;
+
+use RPi::StepperMotor;
+
+my $speed = $ARGV[2] // 'half';
+
+my $s = RPi::StepperMotor->new(
+    pins => [12, 16, 20, 21],
+    speed => $speed
+);
+
+die "need degree arg!\n" if ! $ARGV[0];
+die "need direction!\n" if ! $ARGV[1];
+
+
+if ($ARGV[1] eq 'cw'){
+    $s->cw($ARGV[0]);
+}
+else {
+    $s->ccw($ARGV[0]);
+}
