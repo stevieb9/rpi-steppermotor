176a0417dda1f2bce404f8db6a096c1b98dff37b
Steve Bertrand <steveb@cpan.org>
2018-03-27 14:33:38 +0000

    hashified the four motors in the fork example

--- examples/fork_four_motors.pl
+++ examples/fork_four_motors.pl
@@ -4,47 +4,51 @@ use feature 'say';
 
 use RPi::StepperMotor;
 use Parallel::ForkManager;
- 
+
 my $pm = Parallel::ForkManager->new(4);
- 
-die "need degree arg!\n" if ! $ARGV[0];
-if (! $ARGV[1] || $ARGV[1] !~ /cw/){
-    die "need direction!\n";
+
+help() if ! $ARGV[0];
+my $deg = $ARGV[0];
+my $speed = $ARGV[1] // 'half';
+
+my %motors = (
+    s1 => { pins => [qw(4 17 27 22)] },
+    s2 => { pins => [qw(5 6 13 19)] },
+    s3 => { pins => [qw(18 23 24 25)] },
+    s4 => { pins => [qw(12 16 20 21)] },
+);
+
+my $time = time;
+
+say "starting: $time\n";
+
+for my $motor_name (keys %motors) {
+
+    my $motor = RPi::StepperMotor->new(
+        pins => $motors{$motor_name}{pins},
+        speed => $speed
+    );
+   
+    my $direction = int(rand(2)) ? 'cw' : 'ccw';
+
+    my $pid = $pm->start and next;
+
+    say "$motor_name starting $direction";
+
+    $direction eq 'cw' ? $motor->cw($deg) : $motor->ccw($deg);
+    $motor->cleanup;
+
+    say "$motor_name stopped";
+
+    $pm->finish;
 }
 
-my $speed = $ARGV[2] // 'half';
+$pm->wait_all_children;
 
-my $s1 = RPi::StepperMotor->new(
-    pins => [qw(4 17 27 22)],
-    speed => $speed,
-    name => 's1'
-);
+$time = time;
+say "\nending: $time";
 
-my $s2 = RPi::StepperMotor->new(
-    pins => [qw(5 6 13 19)],
-    speed => $speed,
-    name => 's2'
-);
-
-my $s3 = RPi::StepperMotor->new(
-    pins => [qw(18 23 24 25)],
-    speed => $speed,
-    name => 's3'
-);
-
-my $s4 = RPi::StepperMotor->new(
-    pins => [qw(12 16 20 21)],
-    speed => $speed,
-    name => 's4'
-);
-
-my @motors = ($s1, $s2, $s3, $s4);
-
-for (@motors) {
-    my $pid = $pm->start and next;
-    say $_->name . " starting";
-    $_->cw($ARGV[0]);
-    $_->cleanup;
-    say $_->name . " stopped";
-    $pm->finish;
+sub help {
+    say "Usage: perl fork_four_motors.pl <degrees> [half|full]";
+    exit;
 }
